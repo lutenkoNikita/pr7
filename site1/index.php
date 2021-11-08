f765c7f935b7840e122f5f63e076d63494ec884d
lutenkoNikita <90026027+lutenkoNikita@users.noreply.github.com>
2021-11-08 23:51:17 +0200

    Add files via upload

---
+++ site1/index.php
@@ -0,0 +1,9 @@
+﻿<?php
+	$age = ["Mon" => "8:00", "Tue" => "9:00", "Wed" => "42"];
+	echo "Mon start at " . $age['Mon']."<br>";
+
+	foreach($age as $key => $value) {
+	   echo "Key=" . $key . ", Value=" . $value;
+	   echo "<br>";
+	}			
+?>
